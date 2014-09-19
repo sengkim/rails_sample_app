273dee830b7d3b5616090801f527830bff6733f2
sengkim <1613974287@qq.com>
2014-09-19 21:21:55 +0800

    Finish static pages and full_title helper

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  describe "Home Page" do
+
+    it "should have the h1 'Sample App" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', 
+        :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
+
+end
