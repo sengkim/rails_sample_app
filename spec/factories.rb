68f491a5300fa31c041b5bb1800237373786c5d9
sengkim <1613974287@qq.com>
2014-09-23 20:55:54 +0800

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name      "Sengkim Chanda"
+    email     "sengkim.it@gmail.com"
+    password  "foobar"
+    password_confirmation "foobar"
+  end
+end
